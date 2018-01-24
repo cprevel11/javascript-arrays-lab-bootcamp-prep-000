075bbe218524253070b42cdf330cfa433fccb27b
Christine Prevel <christine.prevel@gmail.com>
2018-01-24 04:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+var kittens = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+ newArray = [...kittens,name]
+ return newArray
+}
+
+function prependKitten(name) { 
+  newArray = [name,...kittens]
+ return newArray
+}
+
+function removeLastKitten() { 
+  removedKitten = kittens.slice(0,kittens.length - 1)
+  return removedKitten
+}
+
+function removeFirstKitten() {
+  kittens.slice(1)
+}
